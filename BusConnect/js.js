c79f2d5171f5710048f99c2b5ab248cf11c86daf
britoalana <110117693+britoalana@users.noreply.github.com>
2023-03-31 10:22:46 -0300

    Add files via upload

---
+++ BusConnect/js.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById("myModal") // obtém o modal
+var btn = document.getElementById("myBtn") // obtém o botão que abre o modal
+var span = document.getElementsByClassName("close")[0] // obtém o elemento <span> que fecha o modal
+
+// quando o usuário clicar no botão, abre o modal
+btn.onclick = function () {
+    modal.style.display = "block"
+}
+
+// quando o usuário clicar no X, feche o modal
+span.onclick = function () {
+    modal.style.display = "none"
+}
